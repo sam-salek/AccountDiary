ef8e72b72b913bb2ac6cf61c46acb994011a2852
Sam Salek <33421876+sam-salek@users.noreply.github.com>
2022-11-26 22:19:17 +0100

    Added init for variables and classes

--- app/src/main/java/com/samsalek/accountdiary/MainActivity.java
+++ app/src/main/java/com/samsalek/accountdiary/MainActivity.java
@@ -3,12 +3,59 @@ package com.samsalek.accountdiary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int numLetters = 29;
+    private final char[] swedishAlphabet = new char[numLetters];
+    private final Map<Character, List<String>> accountMap = new HashMap<>();
+    private ExpandableListView expandableListView;
+    private ExpandableListAdapter expandableListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+
+        populateAlphabet();
+        initAccountMap();
+
+        expandableListView = findViewById(R.id.accountExpandableListView);
+        expandableListAdapter = new AccountExpandableListAdapter(this, swedishAlphabet, accountMap);
+        expandableListView.setAdapter(expandableListAdapter);
+        expandableListView.setOnGroupExpandListener(groupPosition -> {
+
+        });
+
+        accountMap.get(swedishAlphabet[0]).add("Testing");
+    }
+
+    private void populateAlphabet() {
+        int index = 0;
+        for (char alphabet = 'a'; alphabet <= 'z'; alphabet++) {
+            swedishAlphabet[index] = alphabet;
+            index++;
+        }
+
+        swedishAlphabet[index++] = 'å';
+        swedishAlphabet[index++] = 'ä';
+        swedishAlphabet[index] = 'ö';
+    }
+
+    private void initAccountMap() {
+        for (char c : swedishAlphabet)
+            accountMap.put(c, new ArrayList<>());
     }
 }
