cbd9f108d67b3ff4ff9e03eb7b586934633829b7
Sam Salek <33421876+sam-salek@users.noreply.github.com>
2022-12-05 21:44:23 +0100

    Added docs and id to Account

--- app/src/main/java/com/samsalek/accountdiary/Account.java
+++ app/src/main/java/com/samsalek/accountdiary/Account.java
@@ -2,21 +2,46 @@ package com.samsalek.accountdiary;
 
 import android.graphics.Bitmap;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Account {
+public class Account implements Serializable {
 
-    private String name;
-    private List<Bitmap> imageBitmaps = new ArrayList<>();
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Same as the account name, but in lowercase with whitespaces and non-visible characters removed.
+     * Used as an identifier for the account.
+     */
+    private final String id;
+
+    /**
+     * The name used when displaying the account.
+     */
+    private final String name;
+
+    /**
+     * A list of bitmap values for each image associated with the account.
+     */
+    private final List<Bitmap> imageBitmaps = new ArrayList<>();
 
     public Account(String name) {
         this.name = name;
+        this.id = nameToId(name);
+    }
+
+    public static String nameToId(String accountName) {
+        return accountName.replaceAll("\\s+","").toLowerCase();
     }
 
     public void addImageBitmap(Bitmap imageBitmap) {
         imageBitmaps.add(imageBitmap);
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getName() {
